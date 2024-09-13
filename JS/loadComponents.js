058395d8b074b7cadc95a68bb226f8e6ddf63920
simi <joao02simi@gmail.com>
2024-09-13 19:29:51 -0300

    testes dos components header e footer, deu certo tmj

---
+++ JS/loadComponents.js
@@ -0,0 +1,13 @@
+function loadComponent(id, file, selector) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector(selector).innerHTML = data;
+        })
+        .catch(error => console.log('Erro ao carregar o componente:', error));
+}//Essa função carrega os componentes🫦
+
+
+//Carrega os componentes principais aqui, componentes mais específicos devem ser incrementados no script da pagina desejada, por favor deixa organizadinho🫦
+loadComponent('header-placeholder', 'components/header.html', '#header-placeholder'); //header
+loadComponent('footer-placeholder', 'components/footer.html', '#footer-placeholder'); //footer
